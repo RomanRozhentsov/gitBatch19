74da95d502c2870ae31232f76725efe1a6073a8a
Roman <roman.r.rose@gmail.com>
2024-04-06 22:08:14 -0400

    Fifth Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello Mars!");
         System.out.println("Hello");
+        System.out.println("What's up!");
     }
 }
